fad120f87a6f14a8652e8b15575a77079c02980a
Ricardo Geada <ricardogeada.dev@gmail.com>
2023-08-31 15:37:43 +0200

    added mobile template

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    init();
+});
+
+async function init() {
+    await includeHTML();
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let response = await fetch(file);
+        if (response.ok) {
+            element.innerHTML = await response.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
