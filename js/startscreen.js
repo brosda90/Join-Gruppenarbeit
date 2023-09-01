9da810d8310ed52a35d67e2ce817879ce1cd29e8
brosda90 <140012668+brosda90@users.noreply.github.com>
2023-09-01 23:15:54 +0200

    added Sign_up.html

--- js/startscreen.js
+++ js/startscreen.js
@@ -1,25 +1,38 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     setTimeout(() => {
       moveLogoAndShowLogin();
-    }, 500); 
+    }, 600); 
   });
   
   function moveLogoAndShowLogin() {
+    const overlay = document.getElementById("colorOverlay");
     const logoContainer = document.querySelector(".logo-container");
     const logo = document.getElementById("logo");
     const loginContainer = document.getElementById("loginContainer");
   
-    // Logo verschieben und Größe ändern
+    // Logo verschieben 
     logoContainer.style.top = "10px";
     logoContainer.style.left = "10px";
     logoContainer.style.transform = "none";
-    logo.style.width = "100px"; // Zielgröße
-  
+    
+    //Overlay div transparent machen
     setTimeout(() => {
-      // Login-Container anzeigen
-      loginContainer.style.display = "flex";
-    }, 2000); // Warten Sie 2 Sekunden nachdem das Logo verschoben wurde.
-  }
+        overlay.style.transition = "opacity 0.8s ease-out";
+        overlay.style.opacity = "0"; 
+    }, 800); // zeitversetzt starten
+  
+    // Logo ändern und overlay div entfernen wegen mobile touch funktion
+    setTimeout(() => {
+        logo.src = "assets/img/mobile-start-animation-black.svg";
+        overlay.remove();
+        // Login-Container anzeigen
+        loginContainer.style.display = "flex";
+        
+        // Margins für den logoContainer wenn das bild gewandert ist 
+        logoContainer.style.marginTop = "40px";
+        logoContainer.style.marginLeft = "40px";
+    }, 200); // zeitversetzt starten
+}
 
   function openSignUpInNewWindow() {
     window.open("sign-up.html");
