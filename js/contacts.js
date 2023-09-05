1f94e697ffd4be765ca21b8597d0e16ab489f6f4
Alexander Leitermann <alexander@leitermann-home.de>
2023-09-05 15:42:57 +0200

    modify storage.js for return data in json

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,11 @@
+let contactList = [];
+let lastContactId = 0;
+
+
+// ############################################################
+// ----- Vorläufige Funktionen zum Testen
+// ############################################################
+
 function openAddCon() {
     document.getElementById('popup-addcon').classList.toggle('inview');
 }
@@ -24,11 +32,50 @@ function notClose(event) {
 }
 
 
-function saveNewContact() {
-    // New Contact
+// ############################################################
+// ----- Wichtige Funktionen für Alle Contacts-Zugriffe  ------
+// ############################################################
+function idToIndex(id, arr = contactList) { // Die Index-Position im Array aus der ID ermitteln
+    for(let i = 0; i < arr.length; i++) {
+        if(id == arr[i].id) {
+            return i;
+        }
+    }
 }
 
 
-function saveEditContact() {
-    // Edit Contact
+function initialsFrom(string) { // Die Initialen vom Namen erstellen
+    let wordlist = string.split(" ");
+    let words = wordlist.length;
+    let result = '';
+    if(words == 1 && wordlist[0].length >= 2) {
+        for(let i = 0; i < 2; i++) {
+            result += wordlist[0][i];
+        }
+    } else if(words >= 2) {
+        for(let i = 0; i < 2; i++) {
+            result += wordlist[i][0];
+        }
+    }
+    return result.toUpperCase();
+}
+// ############################################################
+// ----- Wichtige Funktionen für Contacts im Allgemeinen ------
+// ############################################################
+async function loadContactsFromStorage() {
+    let loadData = await getItem('contacts');
+    console.log(loadData);
+}
+
+
+
+
+// ############################################################
+function saveNewContact() { // Neuen Kontakt auch im Storage speichern
+    // From New Contact-Popup
+}
+
+
+function saveEditContact() { // Änderung auch im Storage speichern
+    // From Edit Contact-Popup
 }
